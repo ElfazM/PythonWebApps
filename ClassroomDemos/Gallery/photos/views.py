f7ac2bc5c5d195bf9d1b420a0b31f19b291996b0
Elfaz1213 <94564480+Elfaz1213@users.noreply.github.com>
2022-09-14 19:06:13 -0600

    Project 3 edit and changes from project 4

---
+++ ClassroomDemos/Gallery/photos/views.py
@@ -0,0 +1,11 @@
+from decimal import getcontext
+from django.views.generic import TemplateView
+
+class PhotoView(TemplateView): 
+    template_name = 'photo.html'
+
+    def get_context_data(self, **kwargs): # key word argumets 
+        name = kwargs['name']             # gets name from url
+        image = f'\static\images\{name}'  # string that has url which will output whatever name the user asks from the image file
+        return {'photo' : image}          # communicate with template to inject a variable that will point image ehic  wil have the name.
+
