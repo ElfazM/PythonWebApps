52ef685b6fbc1538bd06d4442161a8e4e3d78601
Elfaz1213 <94564480+Elfaz1213@users.noreply.github.com>
2022-09-22 02:18:42 -0600

    Directory cleam up

--- ClassroomDemos/Gallery/config/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from photos.views import PhotoView 
-
-urlpatterns = [
-    path('<str:name>', PhotoView.as_view()), # gets code from photoview  and runs as.view function on it  <str:name what file the user is asking for
-    #    <str:name> gets url you are asking for 
-    # and passes it through the views directory as the name KEY work - kwargs
-]
