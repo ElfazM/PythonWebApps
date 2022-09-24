ab96e063989ed0d2f95f033f81ac3574a37c6720
Elfaz1213 <94564480+Elfaz1213@users.noreply.github.com>
2022-09-24 01:08:55 -0600

    ^^

--- 04/Superhero/heros/views.py
+++ 04/Superhero/heros/views.py
@@ -22,16 +22,13 @@ class heroDetailView(TemplateView):
 def hero_list():
     def hero_details(i, f):
         
-        if   i == 0: caption = f'Guy  ' 
-        elif i == 1: caption = f'Itachi Uchiha ' 
-        elif i == 2: caption = f'Jiraya the sage  ' 
-        elif i == 3: caption = f'Kakashi Sense ' 
-        elif i == 4: caption = f'Minato ' 
-        elif i == 5: caption = f'Naruto Uzamaki ' 
-        elif i == 6: caption = f'Sasuke Uchiha ' 
-
-
-
+        
+        if   i == 0: caption = f'Itachi Uchiha ' 
+        elif i == 1: caption = f'Jiraya the sage  ' 
+        elif i == 2: caption = f'Kakashi Sense ' 
+        elif i == 3: caption = f'Minato ' 
+        elif i == 4: caption = f'Naruto Uzamaki ' 
+        elif i == 5: caption = f'Sasuke Uchiha ' 
         else: None
 
         return dict(id=i, file=f, caption=caption)
