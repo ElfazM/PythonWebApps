1af558286c85ea9d09ae6a0c218b2652e29bae08
Elfaz1213 <94564480+Elfaz1213@users.noreply.github.com>
2022-10-10 00:48:37 -0600

    Login to edit

--- Superhero/hero/urls.py
+++ Superhero/hero/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path
 from django.urls import path, include
 from hero.views import HeroCreateView, HeroDeleteView, HeroDetailView, HeroListView, HeroUpdateView
+
 
 urlpatterns = [
     
@@ -14,5 +14,6 @@ urlpatterns = [
 
     path('accounts/', include('django.contrib.auth.urls')),
 
+    
     path('admin/', admin.site.urls),
 ]
